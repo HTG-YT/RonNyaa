2d003832c3f88b9ae60dcdb40bdd556ca877d7c5
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-02-05 18:16:06 +0800

    update gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,9 @@ repositories {
 }
 
 dependencies {
-    implementation("net.dv8tion:JDA:5.0.0-beta3")
+    implementation("net.dv8tion:JDA:5.0.0-beta.3") {
+        exclude(module = "opus-java")
+    }
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- src/main/java/com/github/htgazurex1212/ronnyaa/Main.java
+++ src/main/java/com/github/htgazurex1212/ronnyaa/Main.java
@@ -1,7 +1,8 @@
 package com.github.htgazurex1212.ronnyaa;
 
+import net.dv8tion.jda.api.JDA;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
     }
 }
