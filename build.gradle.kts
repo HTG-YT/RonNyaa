6090561cd80ae899c6a52fea09c86a1b8bcce8d2
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-02-06 20:24:51 +0800

    add model module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,17 @@
-plugins {
-    id("java")
-}
-
-group = "com.github.htgazurex1212"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    implementation("io.github.cdimascio:dotenv-java:2.3.2")
-    implementation("net.dv8tion:JDA:5.0.0-beta.3") {
-        exclude(module = "opus-java")
+allprojects {
+    repositories {
+        mavenCentral()
     }
-    implementation("org.slf4j:slf4j-jdk14:2.0.6")
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    group = "com.github.htgazurex1212"
 }
 
-tasks.test {
-    useJUnitPlatform()
+subprojects {
+    apply(plugin = "java")
+
+    tasks.withType<JavaCompile> {
+        sourceCompatibility = "19"
+        targetCompatibility = "19"
+        options.encoding = "UTF-8"
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "RonNyaa"
-
+include("discord-frontend")
+include("models")
