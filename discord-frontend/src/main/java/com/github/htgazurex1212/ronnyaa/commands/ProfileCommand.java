23c59bf48170b160d997acb991c732158b13e05c
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-02-07 22:43:01 +0800

    remove unused import

--- discord-frontend/src/main/java/com/github/htgazurex1212/ronnyaa/commands/ProfileCommand.java
+++ discord-frontend/src/main/java/com/github/htgazurex1212/ronnyaa/commands/ProfileCommand.java
@@ -17,7 +17,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.Objects;
 import java.util.Properties;
 
 public class ProfileCommand implements IApplicationCommand {
