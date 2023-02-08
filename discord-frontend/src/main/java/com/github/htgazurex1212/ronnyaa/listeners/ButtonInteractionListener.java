5fded22498215779b51fab98b743c34e740b201b
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-02-08 22:53:07 +0800

    add stuff

---
+++ discord-frontend/src/main/java/com/github/htgazurex1212/ronnyaa/listeners/ButtonInteractionListener.java
@@ -0,0 +1,12 @@
+package com.github.htgazurex1212.ronnyaa.listeners;
+
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class ButtonInteractionListener extends ListenerAdapter {
+    @Override
+    public void onButtonInteraction(@NotNull ButtonInteractionEvent event) {
+        super.onButtonInteraction(event);
+    }
+}
