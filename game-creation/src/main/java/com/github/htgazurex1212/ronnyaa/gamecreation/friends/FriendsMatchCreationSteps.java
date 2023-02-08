616316731e9083435cd07e447860c938e4c34353
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-02-08 22:11:27 +0800

    push to list

--- game-creation/src/main/java/com/github/htgazurex1212/ronnyaa/gamecreation/friends/FriendsMatchCreationSteps.java
+++ game-creation/src/main/java/com/github/htgazurex1212/ronnyaa/gamecreation/friends/FriendsMatchCreationSteps.java
@@ -1,9 +1,15 @@
 package com.github.htgazurex1212.ronnyaa.gamecreation.friends;
 
+import com.github.htgazurex1212.ronnyaa.gamecreation.friends.steps.GameFormatStep;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class FriendsMatchCreationSteps {
     public List<IFriendsMatchCreationStep> steps;
 
-    public FriendsMatchCreationSteps() {}
+    public FriendsMatchCreationSteps() {
+        steps = new ArrayList<>();
+        steps.add(new GameFormatStep());
+    }
 }
