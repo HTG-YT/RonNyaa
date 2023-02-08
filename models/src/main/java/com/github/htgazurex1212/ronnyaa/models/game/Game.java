5b298669020cbd38e65ee91d393ea070aae09ecf
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-02-08 21:29:29 +0800

    add new fields

--- models/src/main/java/com/github/htgazurex1212/ronnyaa/models/game/Game.java
+++ models/src/main/java/com/github/htgazurex1212/ronnyaa/models/game/Game.java
@@ -1,12 +1,19 @@
 package com.github.htgazurex1212.ronnyaa.models.game;
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class Game {
     public @Nullable RoomRank roomRank;
+    public @NotNull GameType gameType;
 
-    public Game(@Nullable RoomRank roomRank) {
+    public Game(@Nullable RoomRank roomRank, @NotNull GameType gameType) {
         this.roomRank = roomRank;
+        this.gameType = gameType;
+    }
+
+    public @NotNull GameType getGameType() {
+        return gameType;
     }
 
     public @Nullable RoomRank getRoomRank() {
---
+++ models/src/main/java/com/github/htgazurex1212/ronnyaa/models/game/GameType.java
@@ -0,0 +1,11 @@
+package com.github.htgazurex1212.ronnyaa.models.game;
+
+public enum GameType {
+    THREE_PLAYER_EAST,
+    THREE_PLAYER_SOUTH,
+    THREE_PLAYER_NORTH,
+
+    FOUR_PLAYER_EAST,
+    FOUR_PLAYER_SOUTH,
+    FOUR_PLAYER_NORTH
+}
