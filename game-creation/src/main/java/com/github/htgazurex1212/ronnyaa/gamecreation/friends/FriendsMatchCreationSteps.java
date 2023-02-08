88fe56b44c9294996b0dcce01dc6992f769cf469
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2023-02-08 22:10:16 +0800

    add constructor and list property

--- game-creation/src/main/java/com/github/htgazurex1212/ronnyaa/gamecreation/friends/FriendsMatchCreationSteps.java
+++ game-creation/src/main/java/com/github/htgazurex1212/ronnyaa/gamecreation/friends/FriendsMatchCreationSteps.java
@@ -1,4 +1,9 @@
 package com.github.htgazurex1212.ronnyaa.gamecreation.friends;
 
+import java.util.List;
+
 public class FriendsMatchCreationSteps {
+    public List<IFriendsMatchCreationStep> steps;
+
+    public FriendsMatchCreationSteps() {}
 }
